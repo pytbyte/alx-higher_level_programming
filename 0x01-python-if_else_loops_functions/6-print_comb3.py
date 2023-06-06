9f2c3b91ea3d90b4b143b8cd0937db7e94f6edb7
pytbyte <heretolearn1@gmail.com>
2023-06-07 00:48:14 +0300

    prints all possible different combinations of two digits.

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 for n1 in range(10):
     for n2 in range(n1 + 1, 10):
-        print("{:02d}, ".format(n1 * 10 + n2), end="")
-
+        if n1 == 8 and n2 == 9:
+            print("{}{}".format(n1, n2))
+        else:
+            print("{}{}".format(n1, n2), end=", ")
