044128839a39f5f1e83963e968db3cdfc78ca15b
pytbyte <heretolearn1@gmail.com>
2023-06-12 11:37:09 +0300

    returns a tuple with the length of a string and its first character

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,7 +2,6 @@
 def multiple_returns(sentence):
     test_tuple = ()
     if len(sentence) == 0:
-        test_tuple = 0
         return None
     else:
         test_tuple = len(sentence), sentence[0]
