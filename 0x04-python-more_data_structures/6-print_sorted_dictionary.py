4f8dd896ccddbfb3b7cee85ba3b2b9da8dffc9ff
pytbyte <heretolearn1@gmail.com>
2023-06-15 05:42:32 +0300

    sorts and prints key and value from a dictonary

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def print_sorted_dictionary(a_dictionary):
+    sorted_items = sorted(a_dictionary.items())
+    for key, value in sorted_items:
+        print(f"{key}: {value}")
