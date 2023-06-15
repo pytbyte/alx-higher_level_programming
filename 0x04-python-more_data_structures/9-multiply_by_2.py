0584f081423e4b246dabd1fde62cd8216ecaac2a
pytbyte <heretolearn1@gmail.com>
2023-06-15 05:59:41 +0300

    multiply dict values by 2

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
     new_dictionary = dict(map(lambda item:
-                            (item[0], item[1] * 2), a_dictionary.items()))
+                              (item[0], item[1] * 2), a_dictionary.items()))
     return new_dictionary
