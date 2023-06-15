27d881e333446f64cd484aa52004fcba7b424218
pytbyte <heretolearn1@gmail.com>
2023-06-15 12:27:10 +0300

    prints all the elements of a dlistint_t list.

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,7 +1,6 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-#include <stddef.h>
 
 /* Structure for doubly linked list */
 typedef struct dlistint_s
@@ -10,8 +9,5 @@ typedef struct dlistint_s
     struct dlistint_s *prev;
     struct dlistint_s *next;
 } dlistint_t;
-
-/* Function prototypes */
-size_t print_dlistint(const dlistint_t *h);
-
+ 
 #endif /* LISTS_H */
