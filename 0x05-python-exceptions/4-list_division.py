ba8485d7df046c635565f701d3297bf8474d6e9f
pytbyte <heretolearn1@gmail.com>
2023-06-26 23:31:57 +0300

    divides element by element 2 lists.

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    result = []
+
+    for i in range(list_length):
+        try:
+            element_1 = my_list_1[i]
+            element_2 = my_list_2[i]
+
+            if not isinstance(element_1, (int, float)) or not isinstance(element_2, (int, float)):
+                print("wrong type")
+                result.append(0)
+                continue
+
+            if element_2 == 0:
+                print("division by 0")
+                result.append(0)
+                continue
+
+            division_result = element_1 / element_2
+            result.append(division_result)
+
+        except IndexError:
+            print("out of range")
+            break
+
+    return result
