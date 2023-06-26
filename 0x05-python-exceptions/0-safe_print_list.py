b010ad6ff510f3d8f7933a76f1a8737eacd1b59c
pytbyte <heretolearn1@gmail.com>
2023-06-26 15:48:39 +0300

    Safe list printing - prints x elements of a list.

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
 
     try:
         for i in range(x):
-            print(my_list[i], end=" ")
+            print(my_list[i], end="")
             element_count += 1
 
     except IndexError:
