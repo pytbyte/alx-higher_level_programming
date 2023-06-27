93cd9e815ed5be47ed5711c02bd07e8639d9c167
pytbyte <heretolearn1@gmail.com>
2023-06-27 23:02:45 +0300

    an empty class Square that defines a square:

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
-#!/usr/bin/python3
 # -*- coding: utf-8 -*-
 """Square Module Docs
 Authour: Kamau Macharia
 Concept: OOP
 """
+
+
 class Square:
     """
     class Square:
-    
-    An empty class that defines a square    
-    
+
+    An empty class that defines a square
     """
     pass
