d85fa7a1eac02dde44658cbf34149e8ad416be5b
pytbyte <heretolearn1@gmail.com>
2023-06-27 23:33:52 +0300

    defines a square with size as  Private instance

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Method Square docs
+   Author : Kamau Macharia
+   Concept : OOP
+"""
+
+
+class Square:
+    """
+    class  Square
+    Defines a modulem called square with size
+    """
+
+    def __init__(self, size):
+        """
+        Private instance attribute: size
+        Instantiation with size
+        (no type/value verification)
+        """
+        pass
