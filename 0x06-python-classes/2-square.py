4ac092100c522690304cd4d03146381b23652e47
pytbyte <heretolearn1@gmail.com>
2023-06-28 00:07:06 +0300

    defines a square with size validation

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+module that writes  constructs 
+a class Square with size validation
+"""
+
+
+class Square:
+    """
+    module that writes  constructs
+    a class Square with size validation
+
+    Attributes:
+        size (int): size field of type int in the Square class. 
+    """
+    def __init__(self, size=0):
+        """
+        This initializes the class and sets the size  attribute
+        to the suplied int from arguments
+
+        This size attribute is a Private instance of the class
+        Square and should always be greater than 0. 
+
+        
+        Args:
+            size (int): the requires parameter.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
