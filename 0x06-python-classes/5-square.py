14c7f3b9d0dc52a7bdd95c3152f6ef0d58b7766a
pytbyte <heretolearn1@gmail.com>
2023-06-28 00:56:16 +0300

    Printing a square

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,91 @@
+#!/usr/bin/python3
+"""
+module that writes  constructs
+a class Square with size validation and
+area culculation.
+"""
+
+
+class Square:
+    """
+    module that writes  constructs
+    a class Square with size validation
+
+    Attributes:
+        size (int): size field of type int in the Square class.
+    """
+    def __init__(self, size=0):
+        """
+        This initializes the class and sets the size  attribute
+        to the suplied int from arguments
+
+        This size attribute is a Private instance of the class
+        Square and should always be greater than 0.
+
+
+        Args:
+            size (int): the requires parameter.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """
+        This Public instance method:
+        def area(self):
+        that returns the current square area.
+
+        Retrieves Private instance attribute: size
+
+        """
+        return self.__size * self.__size
+
+    @property
+    def size(self):
+        """
+        property def size(self):
+        to retrieve Private instance attribute: size
+
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        property setter def size(self, value):
+        to set Private instance attribute: size
+
+        args:
+
+        value
+
+        the int to set as new_size .
+        This updated the Private instance attribute: size
+        with new int.
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def my_print(self):
+        """
+        Public instance method: def my_print(self):
+        prints in stdout the square with the character #:
+
+        args:
+
+        size
+
+        the int to whose square is to be culculated and
+        printed to STDOUT
+        """
+        if self.__size == 0:
+            print("")
+        else:
+            for i in range(self.size):
+                print("#" * self.__size)
