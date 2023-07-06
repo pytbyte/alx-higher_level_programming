b77fc973cfa7cb06651c83fcb8efd4ac9d1ab52c
pytbyte <heretolearn1@gmail.com>
2023-07-06 13:16:38 +0300

    Python - More Classes and Objects late commit

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -132,6 +132,7 @@ class Rectangle:
             return 0
         else:
             return 2 * (self.__width + self.__height)
+
     def __repr__(self):
         """
         return a string representation of the rectangle
