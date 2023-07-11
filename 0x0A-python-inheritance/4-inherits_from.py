610a5e4298f19345f9d74bf610b74b07955203b1
pytbyte <heretolearn1@gmail.com>
2023-07-11 04:21:14 +0300

    class that inherited (directly or indirectly) from the specified class

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -18,4 +18,4 @@ def inherits_from(obj, a_class):
 
     Returns: True or False
     """
-    return isinstance(type(obj), a_class) and type(obj) != a_class
+    return isinstance(obj, a_class) and type(obj) != a_class
