3e506213d5bb840b4a83fb74d6cd91696222badb
pytbyte <heretolearn1@gmail.com>
2023-07-11 05:13:23 +0300

    return priety square area repesentatin

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+module cith class square
+"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    class square with rectangle attributes
+    """
+    def __init__(self, size):
+        """
+        creates square assigning attributes
+        """
+        super().integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """
+        area inherited from area.
+        """
+        return super().area()
+    
+    def __str__(self):
+        """
+        priety representation of the square
+        """
+        return "[Square] {:d}/{:d}".format(self.__size, self.__size)
+    
