f485dd35750bc320855926f2ffbd96e37f8a5921
pytbyte <heretolearn1@gmail.com>
2023-07-11 04:04:00 +0300

    returns True if the object is an instance of a class that inherited (directly or indirectly) from the specified class

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""4-inherits_from.
+returns True if the object is an instance
+of a class that inherited (directly or indirectly)
+from the specified class
+"""
+
+
+def inherits_from(obj, a_class):
+    """4-inherits_from.
+        returns True if the object is an instance
+        of a class that inherited (directly or indirectly)
+        from the specified class
+
+
+    Args:
+       obj: target object
+       a_class: specified class to check
+
+    Returns: True or False
+    """
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
+        return True
+    else:
+        return False
