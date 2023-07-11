21be6096f154408f8e0cdc0d647b9803709fbd65
pytbyte <heretolearn1@gmail.com>
2023-07-11 04:29:16 +0300

    class that inherited (directly or indirectly) from the specified class

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
-"""4-inherits_from
+"""
+4-inherits_from
 returns True if the object is an instance
 of a class that inherited (directly or indirectly)
 from the specified class
+
 """
 
 
 def inherits_from(obj, a_class):
-    """4-inherits_from.
+    """
+    4-inherits_from.
        returns True if the object is an instance
        of a class that inherited (directly or indirectly)
        from the specified class
