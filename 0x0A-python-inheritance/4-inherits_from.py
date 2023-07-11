e07dc4051b09b12dd5af4ffc64bf1ceacda7f256
pytbyte <heretolearn1@gmail.com>
2023-07-11 04:43:07 +0300

    class that inherited (directly or indirectly) from the specified class

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,24 +1,12 @@
 #!/usr/bin/python3
 """
-4-inherits_from
-returns True if the object is an instance
-of a class that inherited (directly or indirectly)
-from the specified class
-
+Module Doc not done yet
 """
 
 
 def inherits_from(obj, a_class):
     """
-    4-inherits_from.
-       returns True if the object is an instance
-       of a class that inherited (directly or indirectly)
-       from the specified class
-
-    Args:
-       obj: target object
-       a_class: specified class to check
-
-    Returns: True or False
+    Function Doc not done yet
     """
+
     return isinstance(obj, a_class) and type(obj) != a_class
