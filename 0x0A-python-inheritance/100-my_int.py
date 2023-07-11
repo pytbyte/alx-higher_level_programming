d002886f28edf6d83c3c2fa51dc447b1f8e7ffad
pytbyte <heretolearn1@gmail.com>
2023-07-11 05:29:28 +0300

    reversed states ie +ve becomes -ve

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -9,13 +9,13 @@ class MyInt(int):
     class that flips
     """
 
-    def __eq__(self, other):
+    def __r__(self, other):
         """
         original is fake
         """
         return self.real != other
 
-    def __ne__(self, other):
+    def __l__(self, other):
         """
         fake is original
         """
