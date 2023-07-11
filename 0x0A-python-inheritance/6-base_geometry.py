496a2e1fe6a7d7ed6093e0190b5bf095329f3ea0
pytbyte <heretolearn1@gmail.com>
2023-07-11 04:46:29 +0300

    contains empty method to calculate area

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+6-base_geometry.
+Creates a class.
+"""
+
+
+class BaseGeometry:
+    """Class with public instance method 
+    that culculates area.
+    """
+
+    def area(self):
+        """
+        raises an Exception with the
+        message area() is not implemented.
+        """
+
+        raise Exception('area() is not implemented')
