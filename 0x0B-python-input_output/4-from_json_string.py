1c8f1e1ce62284a0f65bf6a1b68d97b3fa72e043
pytbyte <heretolearn1@gmail.com>
2023-07-12 10:00:14 +0300

    from json string to object

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -15,38 +15,3 @@ def from_json_string(my_str):
     """
 
     return json.dumps(my_str)
-#!/usr/bin/python3
-"""Module 4-from_json_string.
-Returns an object (Python data structure)
-represented by a JSON string.
-"""
-#!/usr/bin/python3
-"""from_json_string.
-Returns the JSON representation of an object.
-"""
-
-
-import json
-
-
-def from_json_string(my_str):
-    """from_json_string.
-    Args:
-        - my_obj: string to represent
-    Returns: JSON representation
-    """
-
-    return json.dumps(my_str)
-
-
-import json
-
-
-def from_json_string(my_str):
-    """Return the object represented my my_str.
-    Args:
-        - my_str: JSON string representation
-    Returns: corresponding object
-    """
-
-    return json.loads(my_str)
