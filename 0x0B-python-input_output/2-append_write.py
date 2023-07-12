b581b20e2e1a5e4eff1af940166190d813a2ae78
pytbyte <heretolearn1@gmail.com>
2023-07-12 09:39:56 +0300

    append to file

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""_append_file_ : function that creates,
+                opens, appends and
+                prints character count
+"""
+
+
+def append_file(filename="", text=""):
+    """_append_file_ : handles the create
+                    open, append and char
+                    counter.
+        Args:
+        filename (str): the name of the
+                        file you intend
+                        to append to.
+        text (str): the text to append
+                    into the file.
+    """
+
+    with open(filename, 'a') as target:
+        return target.append(text)
