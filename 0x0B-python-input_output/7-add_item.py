4aef4f8c5542e65723482c21b98a786195ee72f2
pytbyte <heretolearn1@gmail.com>
2023-07-12 10:37:01 +0300

    load, add and save

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,15 +1,25 @@
 #!/usr/bin/python3
-"""Add all arguments to a Python list and save them to a file."""
-import sys
+"""
+load and save.
+"""
+from sys import argv
+from os import path
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-if __name__ == "__main__":
-    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file = \
-        __import__('6-load_from_json_file').load_from_json_file
 
-    try:
-        content = load_from_json_file("add_item.json")
-    except FileNotFoundError:
-        content = []
-    content.extend(sys.argv[1:])
-    save_to_json_file(content, "add_item.json")
+def add_items():
+    """
+    function docs
+    """
+    filename = "add_item.json"
+    if path.isfile(filename):
+        final_list = load_from_json_file(filename)
+    else:
+        final_list = []
+    for i in range(1, len(argv)):
+        final_list.append(argv[i])
+    save_to_json_file(final_list, filename)
+
+
+add_items()
