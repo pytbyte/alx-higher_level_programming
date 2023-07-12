854c909d3dd66b72f38e94770d245219e3127c13
pytbyte <heretolearn1@gmail.com>
2023-07-12 09:30:21 +0300

    read from file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,19 +1,19 @@
-#/usr/bin/python3
-"""_read_file_ : function that 
-                opens, reads and 
+#!/usr/bin/python3
+"""_read_file_ : function that
+                opens, reads and
                 prints to stdout
 """
 
 
 def read_file(filename=""):
-    """_read_file_ : handles the 
+    """_read_file_ : handles the
                     read, write and
                     close ops
     Args:
-        filename (str): the name of the 
+        filename (str): the name of the
                         file you intend
                         to read.
     """
     with open(filename) as target:
         content = target.read()
-        print (content, end="")
+        print(content, end="")
