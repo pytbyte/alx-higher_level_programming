520ffcd5c133e819c5fadcbcbbcd0052f7b6d50e
pytbyte <heretolearn1@gmail.com>
2023-07-12 10:08:03 +0300

    load into textfile

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""save_to_json_file.
+writes an Object to a text file,
+using a JSON representation:
+"""
+
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """save_to_json_file.
+    Args:
+        my_obj: string to represent
+        filename: file to write to
+    Returns: JSON representation
+    """
+
+    return json.dumps(my_obj)
