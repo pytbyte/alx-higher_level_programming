216ed19e40c4a381472fea914e7bea10331c2ffc
pytbyte <heretolearn1@gmail.com>
2023-07-17 19:05:16 +0300

    pycodestyle rectifications. this caused errors.

--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 """Defines unittests for models/square.py
 Unittest classes:
-    TestSquare_instantiation 
-    TestSquare_size 
-    TestSquare_x 
-    TestSquare_y 
-    TestSquare_order_of_initialization 
-    TestSquare_area 
-    TestSquare_stdout 
-    TestSquare_update_args 
-    TestSquare_update_kwargs 
+    TestSquare_instantiation
+    TestSquare_size
+    TestSquare_x
+    TestSquare_y
+    TestSquare_order_of_initialization
+    TestSquare_area
+    TestSquare_stdout
+    TestSquare_update_args
+    TestSquare_update_kwargs
     TestSquare_to_dictionary
 """
 import io
