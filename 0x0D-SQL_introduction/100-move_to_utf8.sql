5a217afc14c1761fb88d089d3b3c88fab32c8336
pytbyte <heretolearn1@gmail.com>
2023-08-15 23:58:10 +0300

    alter to UTF8

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,6 +1,6 @@
--- Convert the database character set and collation
--- Convert the field character set and collation
--- Convert the table character set and collation
+-- converts hbtn_0c_0 database to utf8
+-- table: first_table to utf8
+-- field: name to utf8
 
 ALTER DATABASE htbn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
