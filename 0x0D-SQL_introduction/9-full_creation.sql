0d7ead7d2b78df2d4ab8b6f8496005f9a194bc1c
pytbyte <heretolearn1@gmail.com>
2023-08-15 23:14:40 +0300

    create table and add multiple rows

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,12 +1,13 @@
--- create table with multiple rows
--- create table second_table
--- add multiple rows
-CREATE TABLE IF NOT EXISTS second_table (id INT,
-		name VARCHAR(256),
-		score INT
-		);
+-- Create the second_table if it doesn't exist
+CREATE TABLE IF NOT EXISTS hbtn_0c_0.second_table (
+    id INT,
+    name VARCHAR(256),
+    score INT
+);
 
-INSERT INTO second_table (`id`, `name`, `score`) VALUES (1, "John", 10);
-INSERT INTO second_table (`id`, `name`, `score`) VALUES (2, "Alex", 3);
-INSERT INTO second_table (`id`, `name`, `score`) VALUES (3, "Bob", 14);
-INSERT INTO second_table (`id`, `name`, `score`) VALUES (4, "George", 8);
+-- Insert records into the second_table
+INSERT INTO hbtn_0c_0.second_table (id, name, score) VALUES
+    (1, 'John', 10),
+    (2, 'Alex', 3),
+    (3, 'Bob', 14),
+    (4, 'George', 8);
