c9614d41533627bc695ec8700b2277b48dac1a4c
pytbyte <heretolearn1@gmail.com>
2023-08-15 22:28:30 +0300

    Create table

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,3 +1,3 @@
 -- Create table "first_table" in the current database
 
-CREATE IF NOT EXISTS TABLE first_table (id(INT), name(VARCHAR256()));
+CREATE TABLE IF NOT EXISTS first_table (id(INT), name(VARCHAR256()));
