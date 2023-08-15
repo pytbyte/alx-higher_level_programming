43a205f05c9d81be816a77a182929a33fca617cc
pytbyte <heretolearn1@gmail.com>
2023-08-15 23:33:10 +0300

    update score where name is Bob

--- 0x0D-SQL_introduction/12-no_cheating.sql
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -1,2 +1,3 @@
--- lists all records with a score >= 10 in table second_table
-SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
+-- updates the score of Bob to 10 in the table second_table
+-- database name will be passed as an argument of the mysql command
+UPDATE second_table SET score = 10 WHERE name = "Bob";
