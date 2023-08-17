a949e4583da3fd6bd6b9af7ca73ee26fd17c9454
pytbyte <heretolearn1@gmail.com>
2023-08-17 15:32:51 +0300

    top score tunning

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,2 +1,5 @@
---list by best score from second_table
-USE hbtn_0c_0; SELECT score, name FROM second_table ORDER BY score DESC;
+-- Lists all records of the table second_table.
+-- desc
+SELECT `score`, `name`
+FROM `second_table`
+ORDER BY `score` DESC;
