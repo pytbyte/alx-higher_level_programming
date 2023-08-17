65d56a453a98b0eab10a965c45483712c366dfd7
pytbyte <heretolearn1@gmail.com>
2023-08-17 20:38:59 +0300

    My privileges- Show grants comment

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,4 @@
--- List privileges for user_0d_1
--- List privileges for user_0d_2
+-- Lists all privileges of the MySQL Users `user_0d_1`
+-- and `user_0d_2` in `localhost`.
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
