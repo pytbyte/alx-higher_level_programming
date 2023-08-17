94e6fd274f606c37b324a60b03e8b9195ed81d10
pytbyte <heretolearn1@gmail.com>
2023-08-17 22:28:13 +0300

    creates database and user

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,6 @@
--- Creates the database `hbtn_0d_2` and
--- the user `user_0d_2` should have only SELECT privilege in the database.
+-- Creates the database `hbtn_0d_2` 
+-- create user `user_0d_2` 
+-- should have only SELECT privilege in the database.
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 DROP USER IF EXISTS 'user_0d_2'@'localhost';
 CREATE USER 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
