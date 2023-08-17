41a03fe97e96d6d2349896bc6d0ab083d5dbe68b
pytbyte <heretolearn1@gmail.com>
2023-08-17 22:44:31 +0300

    add default value to column unique

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,6 +1,6 @@
 -- Create table `unique_id`
 -- ID default value and unique.
-CREATE TABLE IF NOT EXISTS id_not_null (
+CREATE TABLE IF NOT EXISTS unique_id (
     id INT DEFAULT 1 UNIQUE,
     name VARCHAR(256) NOT NULL
 );
