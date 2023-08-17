521aa11a082891cbd38212940382c0ac5105a47c
pytbyte <heretolearn1@gmail.com>
2023-08-17 22:21:42 +0300

    Root User

---
+++ 0x0E-SQL_more_queries/1-create_useer.sql
@@ -0,0 +1,5 @@
+-- Creates a MySQL Server user `user_0d_1`
+-- set a temporary password `user_0d_1_pwd`.
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost';
+FLUSH PRIVILEGES;
