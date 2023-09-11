35433ffea44bb3ed94dbd904b3bc17e73ea4387e
pytbyte <heretolearn1@gmail.com>
2023-09-11 22:53:54 +0300

    javascript increments

---
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+
+class MyObject {
+    constructor() {
+      this.type = 'object';
+      this.value = 12;
+    }
+  
+    incr() {
+      this.value++;
+    }
+  }
+  
+  const myObject = new MyObject();
+  
+  console.log(myObject);
+  myObject.incr();
+  console.log(myObject);
+  myObject.incr();
+  console.log(myObject);
+  myObject.incr();
+  console.log(myObject);
+  
