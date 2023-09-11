f048367c227d0c4185824363768ad3286152f974
pytbyte <heretolearn1@gmail.com>
2023-09-11 18:16:40 +0300

    javascript args concatation

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,9 +1,3 @@
 #!/usr/bin/node
-const argument1 = process.argv[2];
-const argument2 = process.argv[3];
 
-if (argument1 !== undefined && argument2 !== undefined) {
-  console.log(`${argument1} is ${argument2}`);
-} else {
-  console.log("Missing 2 arguments eg: '4-concat.js arg1 arg2'");
-}
+console.log(process.argv[2] + ' is ' + process.argv[3]);
