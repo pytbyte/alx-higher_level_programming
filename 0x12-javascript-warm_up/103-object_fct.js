2d46639fe5298675fdeb7f0b3211e8f254ae1d58
pytbyte <heretolearn1@gmail.com>
2023-09-11 22:58:04 +0300

    javascript increments

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -1,22 +1,20 @@
 #!/usr/bin/node
 
-class MyObject {
-  constructor () {
-    this.type = 'object';
-    this.value = 12;
-  }
-
-  incr () {
+const myObject = {
+  type: 'object',
+  value: 12,
+  incr: function () {
     this.value++;
   }
-}
-
-const myObject = new MyObject();
+};
 
 console.log(myObject);
+
 myObject.incr();
 console.log(myObject);
+
 myObject.incr();
 console.log(myObject);
+
 myObject.incr();
 console.log(myObject);
