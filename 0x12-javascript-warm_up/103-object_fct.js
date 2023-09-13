d70a660979aafc6bd9da2f80d9a990c732e5e3b5
pytbyte <heretolearn1@gmail.com>
2023-09-13 03:06:22 +0300

    javascript increment correction

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -1,20 +1,17 @@
 #!/usr/bin/node
-const fs = require('fs');
+const myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
 
-if (process.argv.length !== 5) {
-  console.error('Tumia: ./script.js chanzo1.txt chanzo2.txt marudio.txt');
-  process.exit(1);
-}
+myObject.incr = function () {
+  this.value++;
+};
 
-const sourceFilePath1 = process.argv[2];
-const sourceFilePath2 = process.argv[3];
-const destinationFilePath = process.argv[4];
-
-try {
-  const data1 = fs.readFileSync(sourceFilePath1, 'utf8');
-  const data2 = fs.readFileSync(sourceFilePath2, 'utf8');
-  fs.writeFileSync(destinationFilePath, data1 + data2);
-  console.log('Faili zimeunganishwa kwa mafanikio kwenye faili ya marudio.');
-} catch (error) {
-  console.error('Kosa:', error.message);
-}
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
