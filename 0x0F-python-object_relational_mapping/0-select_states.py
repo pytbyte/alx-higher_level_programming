38822e7f0a892b77b5254cb5ce55bf542c0eef9b
pytbyte <heretolearn1@gmail.com>
2023-09-15 18:44:10 +0300

    Python Database manenos

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""_0-select_states.py
+    lists all states from database hbtn_0e_0usa_
+    this program uses MySQLdb
+"""
+import MySQLdb
+conn = MySQLdb.connect(host="localhost", port=3306,
+                       user="root", passwd="root",
+                       db="hbtn_0e_0_usa", charset="utf8")
+cur = conn.cursor()
+
+cur.execute("select * from states ORDER BY id Asc;")
+results = cur.fetchall()
+
+for i in results:
+    print("{}, {}".format(i[0], i[1]))
+
+cur.close()
+conn.close()
---
+++ 0x0F-python-object_relational_mapping/README.md
@@ -0,0 +1,20 @@
+## 0x0F. Python - Object-relational mapping
+
+* Python
+* OOP
+* SQL
+* MySQL
+* ORM
+* SQLAlchemy
+
+# Learning Objectives is to be able to explain:
+
+
+* Why Python programming is awesome
+* How to connect to a MySQL database from a Python script
+* How to SELECT rows in a MySQL table from a Python script
+* How to INSERT rows in a MySQL table from a Python script
+* What ORM means
+* How to map a Python Class to a MySQL table
+* How to create a Python Virtual Environment
+
