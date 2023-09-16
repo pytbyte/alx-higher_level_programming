8990290deab53f75acc6682a7b7be871e475ab55
pytbyte <heretolearn1@gmail.com>
2023-09-16 04:46:16 +0300

    Python Sqlalchemy fetch first entry()

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""7-model_states
+python database manipulation program
+uses Sqlalchemy and some base model
+to query and display the first entry of
+table States in database hbtn_0e_6_usa
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+"""_summary_
+1. handle argv for db connections and error reporting
+2. try create engine and connect
+3. create session
+4. Query data and filter_by first object.
+5. print formated data or throw error message on failiure
+6. close session
+"""
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("./8-modal.. <username><password> <database_name>")
+        sys.exit(1)
+
+    username, password, db_name = sys.argv[1], sys.argv[2], sys.argv[3]
+
+    try:
+        engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                               .format(username, password, db_name),
+                               pool_pre_ping=True)
+
+        Session = sessionmaker(bind=engine)
+        db = Session()
+
+        state_data = db.query(State).order_by(State.id).first()
+
+        if state_data:
+            print("{}: {}".format(state_data.id, state_data.name))
+
+    except Exception as e:
+        print("something went wrong: ", e)
+    finally:
+        db.close()
