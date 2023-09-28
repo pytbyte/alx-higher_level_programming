04ae3c3799f7fc5c71beaec70341f01cc807a4c1
pytbyte <heretolearn1@gmail.com>
2023-09-28 13:48:00 +0300

    python network

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/bash
 #Reads input url from args and sends request to the same.
 #!/bin/bash
-
+# Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
 url="$1"
-curl -s -o /tmp/response.txt "$url"
-stat -c %s /tmp/response.txt
+echo "Size of the response body: $(curl -s "$url" | wc -c) bytes"
