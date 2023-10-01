fad768480878ebe4235967acf024002e86adbc3d
pytbyte <heretolearn1@gmail.com>
2023-10-01 17:50:35 +0300

    python netwwork 1 - Requests error code

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -12,7 +12,6 @@ if __name__ == "__main__":
     url = sys.argv[1]
 
     response = requests.get(url)
-    print(response)
     if response.status_code >= 400:
         print(f"Error code: {response.status_code}")
     else:
