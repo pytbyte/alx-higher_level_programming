200cbde54a93181ff8764137d5fbb5e490930a49
pytbyte <heretolearn1@gmail.com>
2023-10-01 15:34:41 +0300

    python netwwork 1 - urllib Response header value #0

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+"""_1-hbtn_header_
+ takes in a URL
+ sends a request to the URL and
+ displays the value of the X-Request-Id variable
+ found in the header of the response.
+"""
+
+import urllib.request
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: ./script.py <url>")
+        sys.exit(1)
+
+    url = sys.argv[1]
+    try:
+        with urllib.request.urlopen(url) as res:
+            x_request_id = res.getheader('X-Request-Id')
+            if x_request_id:
+                print(x_request_id)
+            else:
+                print("X-Request-Id not found in the response headers.")
+
+    except Exception as e:
+        print("Error :", e)
