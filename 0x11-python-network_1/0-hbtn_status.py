eb5851d446b4cba9683f9852487ade07f7ca2838
pytbyte <heretolearn1@gmail.com>
2023-10-01 14:55:09 +0300

    python netwwork 1 - urllib

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -10,17 +10,18 @@
         - utf8 content: OK$
          
 """
+
 import urllib.request
 
+if __name__ == "__main__":
+    url = 'https://alx-intranet.hbtn.io/status'
 
-url = 'https://alx-intranet.hbtn.io/status'
-
-try:
-    with urllib.request.urlopen(url) as response:
-        body_response = response.read()
-        print("Body response:")
-        print("\t- type:", type(body_response))
-        print("\t- content:", body_response)
-        print("\t- utf8 content:", body_response.decode('utf-8'))
-except Exception as e:
-    print("Error:", e)
+    try:
+        with urllib.request.urlopen(url) as response:
+            body_response = response.read()
+            print("Body response:")
+            print("\t- type:", type(body_response))
+            print("\t- content:", body_response)
+            print("\t- utf8 content:", body_response.decode('utf-8'))
+    except Exception as e:
+        print("Error:", e)
