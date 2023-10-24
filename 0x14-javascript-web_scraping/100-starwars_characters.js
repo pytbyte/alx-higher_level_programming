e8f7aec3ab13ea45e375f714735a4e82765c7708
pytbyte <heretolearn1@gmail.com>
2023-10-24 22:42:57 +0300

    JS update 100

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,38 @@
+#!/usr/bin/node
+const request = require('request');
+
+function fetchMovieCharacters (movieId) {
+  const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+  request(apiUrl, (error, response, body) => {
+    if (error) {
+      console.error(error);
+    } else if (response.statusCode !== 200) {
+      console.error(`Error: ${response.statusCode}`);
+    } else {
+      const movieData = JSON.parse(body);
+      const characterUrls = movieData.characters;
+
+      characterUrls.forEach((characterUrl) => {
+        fetchCharacterName(characterUrl);
+      });
+    }
+  });
+}
+
+function fetchCharacterName (characterUrl) {
+  request(characterUrl, (charError, charResponse, charBody) => {
+    if (!charError && charResponse.statusCode === 200) {
+      const characterData = JSON.parse(charBody);
+      console.log(characterData.name);
+    }
+  });
+}
+
+if (process.argv.length !== 3) {
+  console.error('Usage: ./100-starwars_characters.js <Movie-ID>');
+  process.exit(1);
+}
+
+const movieId = process.argv[2];
+fetchMovieCharacters(movieId);
