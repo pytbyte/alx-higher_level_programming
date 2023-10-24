8d02ce3f7d3c75241e9fea05ecdcf9b5eb222e51
pytbyte <heretolearn1@gmail.com>
2023-10-24 22:36:39 +0300

    JS update 5

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+const fs = require('fs');
+const request = require('request');
+
+function downloadFile (url, filePath) {
+  const fileStream = fs.createWriteStream(filePath);
+
+  request(url)
+    .on('error', (error) => {
+      console.error(error);
+    })
+    .pipe(fileStream);
+}
+
+if (process.argv.length !== 4) {
+  console.error('Usage: ./5-request_store.js <URL> <file-path>');
+  process.exit(1);
+}
+
+const url = process.argv[2];
+const filePath = process.argv[3];
+
+downloadFile(url, filePath);
