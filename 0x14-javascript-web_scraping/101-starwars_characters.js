ffd972f96ac77f8f056ae61a64ad4865bfd2514a
pytbyte <heretolearn1@gmail.com>
2023-10-24 22:45:32 +0300

    JS update 101

---
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,34 @@
+#!/usr/bin/node
+const request = require('request');
+
+function fetchFilmDataAndPrintCharacters (movieId) {
+  const filmUrl = `https://swapi-api.hbtn.io/api/films/${movieId}`;
+
+  request(filmUrl, (error, response, filmBody) => {
+    if (!error && response.statusCode === 200) {
+      const characters = JSON.parse(filmBody).characters;
+      printCharacters(characters, 0);
+    }
+  });
+}
+
+function printCharacters (characters, index) {
+  if (index < characters.length) {
+    const characterUrl = characters[index];
+    request(characterUrl, (error, response, characterBody) => {
+      if (!error && response.statusCode === 200) {
+        const characterData = JSON.parse(characterBody);
+        console.log(characterData.name);
+        printCharacters(characters, index + 1);
+      }
+    });
+  }
+}
+
+if (process.argv.length !== 3) {
+  console.error('Usage: ./script.js <Movie-ID>');
+  process.exit(1);
+}
+
+const movieId = process.argv[2];
+fetchFilmDataAndPrintCharacters(movieId);
